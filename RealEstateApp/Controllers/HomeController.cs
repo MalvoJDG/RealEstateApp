14cbd5d3379f55c9da063ae114ac35e4cc32fc26
Erik Risland Perdomo <20230595@itla.edu.do>
2024-07-22 16:07:45 -0800

    Estructura, bibliotecas y carpetas de la aplicacion.

--- RealEstateApp/Controllers/HomeController.cs
+++ RealEstateApp/Controllers/HomeController.cs
@@ -1,18 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using RealEstateApp.Models;
-using System.Diagnostics;
 
 namespace RealEstateApp.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {//hola
-            _logger = logger;
-        }
-
         public IActionResult Index()
         {
             return View();
@@ -21,12 +12,6 @@ namespace RealEstateApp.Controllers
         public IActionResult Privacy()
         {
             return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
--- RealEstateApp/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace RealEstateApp.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
